be889d70aa9a138fdad8d6c72328e4c603f8ed9c
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-28 16:48:39 -0600

    feat(java): Probamos las funciones del ClientRepository
    Creamos un bloque de codigo en el archivo de entrada para nuestra app con springboot para poder probar rapidamente las funciones de nuestro ClientRepository, asi que creamos un nuevo registro de un Client

--- src/main/java/com/example/examplespringbootjpa/ExampleSpringbootJpaApplication.java
+++ src/main/java/com/example/examplespringbootjpa/ExampleSpringbootJpaApplication.java
@@ -1,13 +1,35 @@
 package com.example.examplespringbootjpa;
 
+import com.example.examplespringbootjpa.model.Client;
+import com.example.examplespringbootjpa.repository.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Timestamp;
 
 @SpringBootApplication
 public class ExampleSpringbootJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringbootJpaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ClientRepository clientRepository) {
+		return args -> {
+			Client client1 = new Client(
+					null,
+					"Josue",
+					true,
+					new Timestamp(System.currentTimeMillis()),
+					"1234",
+					"josue@gmail.com"
+			);
+
+			clientRepository.save(client1);
+		};
 	}
 
 }
