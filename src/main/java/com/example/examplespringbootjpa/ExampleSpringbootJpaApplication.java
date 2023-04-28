1e8b54d54e7bfc3f66b387a5701971a1cec40a21
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-28 14:50:44 -0600

    First Commit

---
+++ src/main/java/com/example/examplespringbootjpa/ExampleSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplespringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examplespringbootjpa/ExampleSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplespringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
